ffd1dcef02b4a7cc13c7999bd952b607ce8df419
Emmanuel Nicolau <emmanicolau@gmail.com>
2022-04-04 21:30:29 +0200

    Make session permanent for the time being to ease development

--- backend/montag/web/app.py
+++ backend/montag/web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session
 from montag.config import Config
 from montag.web import spotify_auth, ytmusic_auth, api
 
@@ -9,4 +9,10 @@ def create_app():
     app.register_blueprint(spotify_auth.bp)
     app.register_blueprint(ytmusic_auth.bp)
     app.register_blueprint(api.bp)
+
+    # Not very secure, but works for now without a full-fledged authentication solution
+    @app.before_request
+    def make_session_permanent():
+        session.permanent = True
+
     return app
